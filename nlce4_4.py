29d615dc7a9c1a87a6b4fdabf1e1cc10466bb1c1
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2014-02-22 12:14:46 -0500

    Added code for writing nlce results to output file and other minor changes

--- nlce4_4.py
+++ nlce4_4.py
@@ -8,32 +8,36 @@ y = 1
 
 ############### To read the data files ##################
 
+alphas = []
+
 # For the Cxx_xx
 while Nx <= n:       #loops from c0202_0101, c0303_0101, ... , C0404_0303
-    fname = "C%02d%02d_%02d%02d.txt"%(Nx,Nx,x,y)     # I used .txt files for sample data
+    fname = "C%02d%02d_%02d%02d"%(Nx,Nx,x,y)
     f = open(fname, 'r')
-    v1 = []
-    v2 = []
+    col1 = []
+    col2 = []
     for lines in f:
         line = lines.split()
-        v1.append(float(line[0]))
-        v2.append(float(line[1]))
+        col1.append(float(line[0]))
+        col2.append(float(line[1]))
     f.close()
+
+    if not alphas: alphas = numpy.array(col1)
 
     # to assign correct labels
     
     # This part gave me trouble. If I put this or the while loop into a seperate function I would
     # lose the label that I had just assigned it here
-    if Nx == 2 and x == 1 and y == 1: C0202_0101 = numpy.array(v2)
-    if Nx == 3 and x == 1 and y == 1: C0303_0101 = numpy.array(v2)
-    if Nx == 3 and x == 1 and y == 2: C0303_0102 = numpy.array(v2)
-    if Nx == 3 and x == 2 and y == 2: C0303_0202 = numpy.array(v2)
-    if Nx == 4 and x == 1 and y == 1: C0404_0101 = numpy.array(v2)
-    if Nx == 4 and x == 1 and y == 2: C0404_0102 = numpy.array(v2)
-    if Nx == 4 and x == 1 and y == 3: C0404_0103 = numpy.array(v2)
-    if Nx == 4 and x == 2 and y == 2: C0404_0202 = numpy.array(v2)
-    if Nx == 4 and x == 2 and y == 3: C0404_0203 = numpy.array(v2)
-    if Nx == 4 and x == 3 and y == 3: C0404_0303 = numpy.array(v2)
+    if Nx == 2 and x == 1 and y == 1: C0202_0101 = numpy.array(col2)
+    if Nx == 3 and x == 1 and y == 1: C0303_0101 = numpy.array(col2)
+    if Nx == 3 and x == 1 and y == 2: C0303_0102 = numpy.array(col2)
+    if Nx == 3 and x == 2 and y == 2: C0303_0202 = numpy.array(col2)
+    if Nx == 4 and x == 1 and y == 1: C0404_0101 = numpy.array(col2)
+    if Nx == 4 and x == 1 and y == 2: C0404_0102 = numpy.array(col2)
+    if Nx == 4 and x == 1 and y == 3: C0404_0103 = numpy.array(col2)
+    if Nx == 4 and x == 2 and y == 2: C0404_0202 = numpy.array(col2)
+    if Nx == 4 and x == 2 and y == 3: C0404_0203 = numpy.array(col2)
+    if Nx == 4 and x == 3 and y == 3: C0404_0303 = numpy.array(col2)
 
     # to cycle through all data
     if y==Nx-1 and x==Nx-1:     #C0303_0202 is the last term required for C0303 data, now move to C0404_0101
@@ -54,21 +58,21 @@ i=False     # this is used so it only adds one additional line term at a time
 
 # For the Lxx_x
 while Nx <= n:       #loops from L0202_01, L0303_01, ... , L0404_0202
-    fname = "L%02d%02d_%02d.txt"%(Nx,Nx,x)     # I used .txt files for sample data
+    fname = "L%02d%02d_%02d"%(Nx,Nx,x)
     f = open(fname, 'r')
-    v1 = []
-    v2 = []
+    col1 = []
+    col2 = []
     for lines in f:
         line = lines.split()
-        v1.append(float(line[0]))
-        v2.append(float(line[1]))
+        col1.append(float(line[0]))
+        col2.append(float(line[1]))
     f.close()
 
     # to assign correct labels
-    if Nx == 2 and x == 1: L0202_01 = numpy.array(v2)
-    if Nx == 3 and x == 1: L0303_01 = numpy.array(v2)
-    if Nx == 4 and x == 1: L0404_01 = numpy.array(v2)
-    if Nx == 4 and x == 2: L0404_02 = numpy.array(v2)
+    if Nx == 2 and x == 1: L0202_01 = numpy.array(col2)
+    if Nx == 3 and x == 1: L0303_01 = numpy.array(col2)
+    if Nx == 4 and x == 1: L0404_01 = numpy.array(col2)
+    if Nx == 4 and x == 2: L0404_02 = numpy.array(col2)
 
         
     # to cycle through all data
@@ -93,6 +97,7 @@ p44 = 2*( C0404_0101 + C0404_0202 + C0404_0303 ) + \
 w4 = p44 - 4*p33 + 7*p22 - 8*p11
 
 # Save result to file
-##f = open('Results_4x4.txt', 'w')
-##f.write( str(w4) )
-##f.close()
+f = open('Results_4x4', 'w')
+for i in range(len(alphas)):
+    f.write("%.20f %.20f\n" % (alphas[i],w4[i]))
+f.close()
