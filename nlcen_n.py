2a97da9845937aec07e0b9917e30fbe3aaa9d526
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2014-03-18 14:48:41 -0400

    Redefined results of nlcen_n.py to be partial sums of the weights up to a specified order.

--- nlcen_n.py
+++ nlcen_n.py
@@ -2,16 +2,18 @@ import numpy
 import getdatafile    # read data
 import weightfile     # performs cluster weight calculations
 
+maxn = 6     # square lattice nxn
 
-n = 4     # square lattice nxn
+total = None
+for n in range(2,maxn+1):
+    d, alphas = getdatafile.getdata(n)
+    w_n = weightfile.weight(n,d)
 
+    if total is None: total = w_n
+    else: total += w_n
 
-d, alphas = getdatafile.getdata(n)
-w_n = weightfile.weight(n,d)
-
-
-# Save result to file
-f = open('Results_nxn', 'w')
-for i in range(len(alphas)):
-    f.write("%.20f %.20f\n" % (alphas[i],w_n[i]))
-f.close()
+    # Save result to file
+    f = open("Results_%02dx%02d" % (n,n), 'w')
+    for i in range(len(alphas)):
+        f.write("%.20f %.20f\n" % (alphas[i],total[i]*0.5))
+    f.close()
