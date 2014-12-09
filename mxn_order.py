9246da20b865d62df1eff5f3b445bf6fe0e3b011
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2014-12-09 10:20:03 -0500

    Took a stab at adding Geometric order, needs testing

--- mxn_order.py
+++ mxn_order.py
@@ -1,7 +1,6 @@
 
 class Arithmetic:
 
-
     def length(self,N):
         return (2+N)/2.
 
@@ -16,4 +15,23 @@ class Arithmetic:
             x -= 1
             y += 1
             res.append((x,y))
+        return res
+
+class Geometric:
+
+    def length(self,L):
+        return L
+
+    def lengthstr(self,L):
+        return "%.1f"%self.length(L)
+
+    def clusters(self,L):
+        from math import sqrt
+        from frange import frange
+        res = []
+        for y in frange(2,L+1.):
+            for x in frange(y,L+1.):
+                ll = sqrt(x*y)
+                if (ll > (L-0.5) or abs(ll-(L-0.5))<1E-5) and (ll < L or abs(ll-L)<1E-5):
+                    res.append((int(x),int(y)))
         return res
